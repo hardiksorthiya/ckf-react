8b39103dd026ffb8c9c6837813fb8d4af4223f95
Hardik Sorathiya <sorathiyahardik000@gmail.com>
2025-04-11 11:36:11 +0530

    basename chang in browserroute

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
   AOS.init({
   }); 
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/ckf-react">
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/home" element={<Home />} />
